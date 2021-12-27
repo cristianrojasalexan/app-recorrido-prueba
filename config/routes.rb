69422a3f6dbead5f703b1c27dd926860c6f7b303
cristianrojasalexan <cristianrojas.ca@gmail.com>
2021-12-27 16:24:57 -0300

    add login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  authenticate :user do
+    root "availability#index", as: :authenticated_root
+  end
+  devise_scope :user do
+    root to: "users/sessions#new"
+  end
 end
