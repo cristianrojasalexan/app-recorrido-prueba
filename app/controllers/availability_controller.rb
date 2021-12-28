2c70ed16a90a9de4d1bc11e6d22ac9f2121eddb2
cristianrojasalexan <cristianrojas.ca@gmail.com>
2021-12-28 06:48:59 -0300

    add availability to user, work in progres

--- app/controllers/availability_controller.rb
+++ app/controllers/availability_controller.rb
@@ -1,4 +1,15 @@
 class AvailabilityController < ApplicationController
     def index
     end
+    def create
+        puts params
+    end
+
+    def select_week
+        puts "-----------#{params[:data][:week]}"
+        @week_selected = params[:data][:week].to_i
+        respond_to do |format|
+            format.js
+        end
+    end
 end
--- app/helpers/availability_helper.rb
+++ app/helpers/availability_helper.rb
@@ -1,2 +1,28 @@
 module AvailabilityHelper
+    
+    def weekend_schedule
+        ["10:00 - 11:00","11:00 - 12:00","12:00 - 13:00","13:00 - 14:00","14:00 - 15:00","15:00 - 16:00",
+        "16:00 - 17:00","17:00 - 18:00","18:00 - 19:00","19:00 - 20:00", "20:00 - 21:00", "21:00 - 22:00", 
+        "22:00 - 23:00", "23:00 - 00:00"]
+    end
+    
+    def week_schedule
+        ["19:00 - 20:00", "20:00 - 21:00", "21:00 - 22:00", "22:00 - 23:00", "23:00 - 00:00"]
+    end
+
+    def beginning_of_the_week(date)
+        (Time.now + ((date.strftime('%V').to_i) * 7).days).beginning_of_week
+    end
+
+    def end_of_the_week(date)
+        (Time.now + ((date.strftime('%V').to_i) * 7).days).end_of_week
+    end
+
+    def get_time_weekend(date, time)
+        date += 19.hour + time.hour
+    end
+
+    def get_time_week(date, time)
+        date += 10.hour + time.hour
+    end
 end
